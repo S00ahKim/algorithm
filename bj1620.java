cb5f2cde21b70a5473d8399f234c2c69f782835d
S00ahKim <rlatndk0206@naver.com>
2019-03-08 16:35:52 +0900

    #1620

--- bj1620.java
+++ bj1620.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Pokemon {
+public class bj1620 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
