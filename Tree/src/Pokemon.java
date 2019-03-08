541faede3a9d6026e3cb86b3b97c3993a34443d4
S00ahKim <rlatndk0206@naver.com>
2019-03-08 11:52:43 +0900

    #1620

---
+++ Tree/src/Pokemon.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Pokemon {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		HashMap<String, Integer> strMaps = new HashMap<>();
+		HashMap<Integer, String> intMaps = new HashMap<>();
+		
+		for (int i=1; i<=N; i++) {
+			String s = sc.next();
+			strMaps.put(s, i);
+			intMaps.put(i, s);
+		}
+		
+		for (int i=0; i<M; i++) {
+			if (sc.hasNextInt()) {
+				System.out.println(intMaps.get(sc.nextInt()));
+			}else {
+				System.out.println(strMaps.get(sc.next()));
+			}
+		}
+		sc.close();
+		
+	}
+
+}
